19af63b2223174297f8fdc46ba425ceb8d1378f1
Tim Holy <tim.holy@gmail.com>
2015-01-06 06:27:28 -0600

    Add a more stringent coverage test

---
+++ test/data/testparser.jl
@@ -0,0 +1,5 @@
+f1(x) = 2x
+if isdefined(:f1)
+  f2(x) = 3x
+end
+f3(x) = 4x
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,12 @@
 # https://github.com/IainNZ/Coverage.jl
 #######################################################################
 
-using Coverage
+using Coverage, Base.Test
 
-cd(Pkg.dir("Coverage"))
-j = Coveralls.process_file(joinpath("test","data","Coverage.jl"))
+cd(Pkg.dir("Coverage")) do
+    j = Coveralls.process_file(joinpath("test","data","Coverage.jl"))
+end
+
+run(`julia --code-coverage=user -e 'include("data/testparser.jl"); using Base.Test; @test f1(2) == 4'`)
+r = Coveralls.process_file(joinpath("data","testparser.jl"))
+@test r["coverage"][1:5] == [1,nothing,0,nothing,0]
