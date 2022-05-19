65f044f5fc2f3b8d1610a7edce29558f2c25d975
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-05-19 16:22:01 +0200

    Add Gitlab to Coveralls

--- src/coveralls.jl
+++ src/coveralls.jl
@@ -105,6 +105,10 @@ function prepare_request(fcs::Vector{FileCoverage}, local_env::Bool, git_info=qu
         github_pr = get(github_pr_info, "number", "")
         github_pr::Union{AbstractString, Integer}
         ((github_pr isa Integer) || (!isempty(github_pr))) && (data["service_pull_request"] = strip(string(github_pr)))
+    elseif haskey(ENV, "GITLAB_CI")
+        data["service_job_id"] = ENV["CI_JOB_ID"]
+        data["service_name"] = "gitlab"
+        data["git"] = parse_git_info(git_info)
     else
         data["git"] = parse_git_info(git_info)
     end
